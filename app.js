3e6d2df1f812288c334bba5d9456b823fa8c6818
jinyeopoh <jinyeopoh96@gmail.com>
2023-06-23 16:41:40 +1000

    app.js and a few dependencies added

---
+++ app.js
@@ -0,0 +1,18 @@
+// Configuration
+const express = require("express");
+const path = require('path')
+const app = express();
+
+app.listen(8080);
+app.use('/', express.static(path.join(__dirname, 'dist/resume-app')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Angular configures # in URL. This requires no configuration on server side.
+// In order to have no # in the URL, set urlHash: false in app-routing.module.ts
+// Then, as below, accept all request and simply render the index.html
+app.use('/', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    const filePath = path.join(__dirname, 'dist/resume-app')
+    res.sendFile('index.html', { root: filePath });
+});
